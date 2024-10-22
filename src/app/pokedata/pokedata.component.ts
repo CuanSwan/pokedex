03daf5a1cfc422d988ca4bb07bed87b0ffad9657
CuanSwan <cuan@itonlinelearning.com>
2024-10-22 09:23:43 +0100

    Added extra styling

--- src/app/pokedata/pokedata.component.ts
+++ src/app/pokedata/pokedata.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PokepicComponent } from '../pokepic/pokepic.component';
+import { DescriptionComponent } from '../description/description.component';
 
 @Component({
   selector: 'app-pokedata',
   standalone: true,
-  imports: [],
+  imports: [PokepicComponent, DescriptionComponent],
   templateUrl: './pokedata.component.html',
   styleUrl: './pokedata.component.css'
 })
